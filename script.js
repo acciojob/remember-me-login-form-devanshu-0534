70263b3d6ffaa5d5a0ac1b7504a3d37b02303022
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2026-01-13 16:57:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const submitBtn = document.getElementById("submit");
+const existingBtn = document.getElementById("existing");
+const form = document.getElementById("loginForm");
+
+
+ window.addEventListener("load", () => {
+      const savedUsername = localStorage.getItem("username");
+      const savedPassword = localStorage.getItem("password");
+
+      if (savedUsername && savedPassword) {
+        existingBtn.style.display = "inline-block";
+      }
+    });
+
+    // Handle form submission
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const username = usernameInput.value;
+      const password = passwordInput.value;
+
+      alert(`Logged in as ${username}`);
+
+		     if (checkbox.checked) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+        existingBtn.style.display = "inline-block";
+      } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+        existingBtn.style.display = "none";
+      }
+    });
+
+    // Login as existing user
+    existingBtn.addEventListener("click", () => {
+      const savedUsername = localStorage.getItem("username");
+      alert(`Logged in as ${savedUsername}`);
+    });
+
+
+		
